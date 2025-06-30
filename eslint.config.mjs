45beba53a0c68ff229cb636961101b8db574d8dc
Eloho Kennedy <kennyduske@gmail.com>
2025-06-30 11:30:24 +0100

    ignore build errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,10 +10,8 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript', {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-    },
-  }),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {},
 ];
+
 export default eslintConfig;
