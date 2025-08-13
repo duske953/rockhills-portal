ac654b5d45e557589d87f31d857d2a94138bc1f6
Eloho Kennedy <kennyduske@gmail.com>
2025-08-13 20:55:44 +0100

    update room amount

--- app/utils/isValidAmount.ts
+++ app/utils/isValidAmount.ts
@@ -6,7 +6,7 @@ export default function validAmount(
   amount: number
 ) {
   if (validStayType?.stay === 'FULL-TIME') {
-    return validRoom?.price === amount;
+    return validRoom?.price >= amount;
   }
   if (validStayType?.stay === 'SHORT-REST') {
     return amount >= MIN_AMOUNT && amount <= MAX_AMOUNT;
