8caea71855884ccf29896c84d696079c48612da7
Eloho Kennedy <kennyduske@gmail.com>
2025-08-13 23:27:46 +0100

    fix input amount

--- app/portal/components/RegisterCustomer.tsx
+++ app/portal/components/RegisterCustomer.tsx
@@ -113,7 +113,7 @@ export default function RegisterCustomer() {
         <DropDownStayType setStayType={setStayType} stayType={stayType} />
         <Input
           value={roomMetaData.amount}
-          type="text"
+          type="number"
           onChange={(e) => renderRoomMetaDataChange(e, 'amount')}
           placeholder="Enter Amount"
         />
